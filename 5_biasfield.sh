3e9ad1c1709985c10e0a4b114f5b25e22687fdbc
Evan H <ehare@usc.edu>
2019-08-27 12:11:24 -0700

    bias field +dtifit

--- 5_biasfield.sh
+++ 5_biasfield.sh
@@ -3,6 +3,15 @@
 
 
 
-# BIAS FIELD CORRECTION
+# BIAS FIELD CORRECTION -------------------
 
+# current problem with script: error in one of the called functions within mrtrix3
+
+export PATH=“$HOME/software/mrtrix3/bin:/$HOME/software/mrtrix3/lib:/$HOME/software/mrtrix3/share:$PATH”
+export LD_LIBRARY_PATH=/ifshome/dmoyer/gcc-5.2.0/lib64:${LD_LIBRARY_PATH}
+export PATH=/ifs/loni/faculty/thompson/four_d/igc_basics/anaconda3/bin:$PATH
+export ANTSPATH=/usr/local/ANTs_2.2.0/bin/bin/
+PATH=${ANTSPATH}:$PATH 
+
+/ifshome/ehaddad/software/mrtrix3/bin/dwibiascorrect ${Input} ${Output}_d1corr_nii.gz -ants -mask ${mask} -bias ${output}_bias.nii.gz -fslgrad ${bvec} ${bval} -tempdir ${temp_dir}
 
---
+++ 6_dtifit.sh
@@ -0,0 +1,10 @@
+## DTI FIT -------
+
+CONFIG="dti.config"
+
+bvec=${dir}/${subj}/output/${subj}.bvec
+bval=${dir}/${subj}/output/${subj}.bval
+
+${DTIFIT_PATH} -w -k ${dir}/${subj}/output/${subj}_d1corr_nii.gz -o ${dir}/${subj}/output/${subj}_dtifit -m ${dir}/${subj}/output/${subj}_dwi_brain_mask.nii.gz -r ${bvec} -b ${bval} --wls --save_tensor
+
+
